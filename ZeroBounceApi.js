3f9e8dbf8ee60bd2ec3fc11253b2749abf7c2246
ZeroBounce <34064209+zerobounce@users.noreply.github.com>
2018-07-17 15:27:14 -0400

    Create ZeroBounceApi.js

---
+++ ZeroBounceApi.js
@@ -0,0 +1,37 @@
+class ZeroBounceApi {
+    constructor(apiKey){
+        var baseUrl = "https://api.zerobounce.net/v2";
+        var get = new XMLHttpRequest();
+        /**
+         * @param apiKey - your private API key
+         * */
+        this.apiKey = apiKey;
+
+        /**
+         * @return the number of credits remaining on your account
+         * */
+        this.getCredits = function(){
+            var uri = baseUrl + "/getcredits" + "?api_key=" + apiKey;
+            get.open('GET', uri, false);
+            get.send();
+            if (get.readyState === 4 && get.status === 200) {
+                return get.responseText;
+            }
+        }
+
+
+        /**
+         * @param email - the email you want to validate
+         * @param ip - the ip to be use for this validation (optional)
+         * @return - a JSONObject with all of the information for the specified email
+         * */
+        this.validate = function(email, ip_address){
+            var uri = baseUrl + "/validate" + "?api_key=" + apiKey + "&email=" + email + "&ip_address=" + ip_address;
+            get.open('GET', uri, false);
+            get.send();
+            if (get.readyState == 4 && get.status == 200) {
+                return get.responseText;
+            }
+        }
+    }
+}
